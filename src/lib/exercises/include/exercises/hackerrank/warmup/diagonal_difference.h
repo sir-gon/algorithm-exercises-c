763faf9ab39ffe7020a70091671fd2da445a286f
Gonzalo Diaz <devel@gon.cl>
2024-11-15 12:16:20 -0300

    [Hacker Rank]: Warmup: Diagonal Difference. Solved ✅.

---
+++ src/lib/exercises/include/exercises/hackerrank/warmup/diagonal_difference.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int HACKERRANK_WARMUP_diagonalDifference(int arr_rows, int arr_columns,
+                                         int **arr);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
