e90aeede632ace9bcf5f93342beacc7c98dd9562
Gonzalo Diaz <devel@gon.cl>
2026-06-16 16:45:54 -0400

    Test coverage increased due new malloc() interceptor.

--- src/tests/unit/lib/hackerrank/warmup/mini_max_sum.test.cpp
+++ src/tests/unit/lib/hackerrank/warmup/mini_max_sum.test.cpp
@@ -1,8 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "../../../../tools/memory_injector.hpp"
 #include <exercises/hackerrank/warmup/mini_max_sum.h>
-#include <iostream>
-#include <stdexcept>
 #include <string>
 #include <vector>
 
@@ -16,36 +15,53 @@ using json = nlohmann::json;
 
 TEST_CASE("miniMaxSum JSON Test Cases",
           "[hackerrank] [jsontestcase] [warmup]") {
-  std::filesystem::path cwd = std::filesystem::current_path();
-  std::string path =
-      cwd.string() + "/unit/lib/hackerrank/warmup/mini_max_sum.testcases.json";
+#ifdef __linux__
+  SECTION("miniMaxSum: Failure handling during Out of Memory (OOM)") {
+    MemoryInjector::enable_oom_fault();
 
-  INFO("miniMaxSum JSON test cases FILE: " << path);
+    int input_size = 3;
+    int input_array[] = {1, -2, 0};
 
-  std::ifstream f(path);
-  json data = json::parse(f);
-
-  for (auto testcase : data) {
-    auto input_size = static_cast<int>(testcase["input"].size());
-    std::vector<int> input_vector = testcase["input"];
-    const int *input_array = input_vector.data();
-
-    char *result =
-        HACKERRANK_WARMUP_miniMaxSumCalculate(input_size, input_array);
     HACKERRANK_WARMUP_miniMaxSum(input_size, input_array);
 
-    std::string result_as_string(result);
-
-    free(result);
-
-    CHECK(result_as_string == testcase["expected"]);
+    // Clean up the state after finishing the test section
+    MemoryInjector::disable_fault();
   }
-}
+#endif
 
-TEST_CASE("miniMaxSum Edge Cases", "[hackerrank] [warmup]") {
-  std::vector<int> empty({});
+  SECTION("miniMaxSum: Normal memory allocation succeeds") {
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::string path =
+        cwd.string() +
+        "/unit/lib/hackerrank/warmup/mini_max_sum.testcases.json";
 
-  const char *result = HACKERRANK_WARMUP_miniMaxSumCalculate(0, empty.data());
+    INFO("miniMaxSum JSON test cases FILE: " << path);
 
-  CHECK(result == nullptr);
+    std::ifstream f(path);
+    json data = json::parse(f);
+
+    for (auto testcase : data) {
+      auto input_size = static_cast<int>(testcase["input"].size());
+      std::vector<int> input_vector = testcase["input"];
+      const int *input_array = input_vector.data();
+
+      char *result =
+          HACKERRANK_WARMUP_miniMaxSumCalculate(input_size, input_array);
+      HACKERRANK_WARMUP_miniMaxSum(input_size, input_array);
+
+      std::string result_as_string(result);
+
+      free(result);
+
+      CHECK(result_as_string == testcase["expected"]);
+    }
+  }
+
+  SECTION("miniMaxSum Edge Cases", "[hackerrank] [warmup]") {
+    std::vector<int> empty({});
+
+    const char *result = HACKERRANK_WARMUP_miniMaxSumCalculate(0, empty.data());
+
+    CHECK(result == nullptr);
+  }
 }
--- src/tests/unit/lib/hackerrank/warmup/time_conversion.test.cpp
+++ src/tests/unit/lib/hackerrank/warmup/time_conversion.test.cpp
@@ -1,41 +1,56 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "../../../../tools/memory_injector.hpp"
 #include <exercises/hackerrank/warmup/time_conversion.h>
 #include <filesystem>
 #include <fstream>
 #include <nlohmann/json.hpp>
-#include <vector>
 
 using json = nlohmann::json;
 
 TEST_CASE("time_conversion JSON Test Cases",
           "[hackerrank] [jsontestcase] [warmup]") {
-  std::filesystem::path cwd = std::filesystem::current_path();
-  std::string path =
-      cwd.string() +
-      "/unit/lib/hackerrank/warmup/time_conversion.testcases.json";
+#ifdef __linux__
+  SECTION("time_conversion Failure handling during Out of Memory (OOM)") {
+    MemoryInjector::enable_oom_fault();
 
-  INFO("time_conversion JSON test cases FILE: " << path);
+    const char *input = "12:01:00PM";
 
-  std::ifstream f(path);
-  json data = json::parse(f);
+    REQUIRE(HACKERRANK_WARMUP_timeConversion(input) == nullptr);
 
-  for (auto testcase : data) {
-    char *result = HACKERRANK_WARMUP_timeConversion(
-        testcase["input"].get<std::string>().c_str());
-
-    std::string result_as_string(result);
-
-    free(result);
-
-    CHECK(result_as_string == testcase["expected"]);
+    // Clean up the state after finishing the test section
+    MemoryInjector::disable_fault();
   }
-}
+#endif
 
-TEST_CASE("time_conversion edge cases", "[hackerrank] [helper] [warmup]") {
-  CHECK(HACKERRANK_WARMUP_timeConversion(nullptr) == nullptr);
+  SECTION("Normal memory allocation succeeds") {
+    std::filesystem::path cwd = std::filesystem::current_path();
+    std::string path =
+        cwd.string() +
+        "/unit/lib/hackerrank/warmup/time_conversion.testcases.json";
 
-  CHECK(HACKERRANK_WARMUP_timeConversion("") == nullptr);
+    INFO("time_conversion JSON test cases FILE: " << path);
 
-  CHECK(HACKERRANK_WARMUP_timeConversion("aa:bb:ccXM") == nullptr);
+    std::ifstream f(path);
+    json data = json::parse(f);
+
+    for (auto testcase : data) {
+      char *result = HACKERRANK_WARMUP_timeConversion(
+          testcase["input"].get<std::string>().c_str());
+
+      std::string result_as_string(result);
+
+      free(result);
+
+      CHECK(result_as_string == testcase["expected"]);
+    }
+  }
+
+  SECTION("time_conversion edge cases", "[hackerrank] [helper] [warmup]") {
+    CHECK(HACKERRANK_WARMUP_timeConversion(nullptr) == nullptr);
+
+    CHECK(HACKERRANK_WARMUP_timeConversion("") == nullptr);
+
+    CHECK(HACKERRANK_WARMUP_timeConversion("aa:bb:ccXM") == nullptr);
+  }
 }
